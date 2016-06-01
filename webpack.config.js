79c6b8175a8784520222c8021db9519518b57c79
Timon Sotiropoulos <timon@seedtech.io>
2016-06-01 15:46:43 +1000

    Added slight work around to get the production version of teh code running using webpack?

--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,10 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 // Setup the React Hot Loader if we are working in the development environment
 if (process.env.NODE_ENV === 'development') {
-    console.log("Running Development");
     var loaders = ['react-hot', 'babel'];
 } else {
     var loaders = ['babel'];
 }
-
 // Here we create the path that our bundled file will be saved too
 fs.mkdir('./src/jsx', 0777, function(err) {
     if (!err) {
@@ -22,6 +20,7 @@ fs.mkdir('./src/jsx', 0777, function(err) {
 });
 // Definitng the output path for our bundle file
 var outputPath = __dirname + '/src/jsx'
+
 
 // The modules we are using
 module.exports = {
@@ -37,16 +36,19 @@ module.exports = {
             test: /\.js$/,
             loaders: loaders,
             exclude: /node_modules/
-        },{
-            test: /\.scss/,
-            loader: ExtractTextPlugin.extract(["css","postcss", "sass"])
+        },
+        {
+            test: /\.scss$/,
+            loader: 'style!css!sass' // For use with the webpack dev server
+            // loader: ExtractTextPlugin.extract(["css","postcss", "sass"]) // For use with the production server (Eg splitting out the css file)
+
         }],
     },
     postcss: [ autoprefixer({ browsers: ['last 3 versions'] }) ],
     plugins: [
         new ExtractTextPlugin("../css/styles.css", {
             allChunks: true,
-            disable: false
+            disable: true
         })
     ],
 
